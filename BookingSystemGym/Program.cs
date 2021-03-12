c27a5e1db30939ddaa66dcbd109c2a0988146dc2
LindaHolmberg <69193585+LindaHolmberg@users.noreply.github.com>
2021-03-12 12:22:54 +0100

    commit things

--- BookingSystemGym/Program.cs
+++ BookingSystemGym/Program.cs
@@ -104,7 +104,8 @@ namespace BookingSystemGym
                         }
                         else
                         {
-                            bs.CurrentUser.MakeReservation(ST[int.Parse(input)-1]);
+                            bs.CurrentUser.MakeReservation(ST[int.Parse(input) - 1]); 
+                            Console.WriteLine("Din bokning har genomförts! Välkommen! "); 
                         }
                         
 
